d032f69def662d48d8f7f70418a9c8e38b3e7032
liuhongjiang <andrew.lhj@gmail.com>
2012-12-06 17:20:45 +0800

    jni

---
+++ cpp_test/jni/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "### compile Sample1.java"
+javac Sample1.java
+
+echo "### generate jni headerfile Sample1.h"
+javah -classpath ./ -jni Sample1
+
+echo "### compile Sample1.so"
+g++ -I /usr/lib/jvm/java-1.6.0-openjdk/include/ -I /usr/lib/jvm/java-1.6.0-openjdk/include/linux/ Sample1.cpp -fPIC -shared -o libSample1.so
+
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
+echo "### run"
+echo "---------------"
+java Sample1
+
+#rm libSample1.so Sample1.class Sample1.h
+
