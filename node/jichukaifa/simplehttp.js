117632b4a93661fc3c597624169570ca320aa5ef
andrewliu <andrew.liu@qq.com>
2013-10-22 23:21:01 +0800

    node 基础

---
+++ node/jichukaifa/simplehttp.js
@@ -0,0 +1,10 @@
+var http = require("http")
+
+http.createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.write('<h1>Node.js</h1>');
+	res.write('<p>Hello world</p>');
+	res.end();
+}).listen(8888);
+
+console.log("HTTP server is started at port 8888:");
