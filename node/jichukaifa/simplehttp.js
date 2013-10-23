f52985bb2709438a8f6c8d0d681c3ae2a66e51cb
andrewliu <andrew.liu@qq.com>
2013-10-23 22:40:06 +0800

    add gitignore

--- node/jichukaifa/simplehttp.js
+++ node/jichukaifa/simplehttp.js
@@ -3,7 +3,7 @@ var http = require("http")
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type':'text/html'});
 	res.write('<h1>Node.js</h1>');
-	res.write('<p>Hello world</p>');
+	res.write('<p>Hello world! lhj</p>');
 	res.end();
 }).listen(8888);
 
